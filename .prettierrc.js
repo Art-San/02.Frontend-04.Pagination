69886ce19581c955839d16ed610a30561f9f96a3
Aleksander <asava@sibmail.com>
2023-03-22 22:50:11 +0700

    Eslint настроен и PropTypes  назначены

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,17 @@
 module.exports = {
-	tabWidth: 4, // ширина таб пробелы 4
-	singleQuote: true, // одинарные ковычки
-	jsxSingleQuote: true, // jsx одинарные ковычки
-	arrowParens: 'avoid', // если стрелочная функция и один аргумент то не будет скобок
-	semi: false, // нет точки с запятой в конце
-	trailingComma: 'none', // это последня запятая
-	useTabs: true, // Делайте отступы с помощью табуляции вместо пробелов.
-	bracketSameLine: false
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true
 }
+
+// module.exports = {
+// 	tabWidth: 2, // ширина таб пробелы 4
+// 	singleQuote: true, // одинарные ковычки
+// 	jsxSingleQuote: true, // jsx одинарные ковычки
+// 	// arrowParens: 'avoid', // если стрелочная функция и один аргумент то не будет скобок
+// 	semi: false, // нет точки с запятой в конце
+// 	trailingComma: 'none', // это последня запятая
+// 	useTabs: true, // Делайте отступы с помощью табуляции вместо пробелов.
+// 	bracketSameLine: false
+// }
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,31 +4,31 @@ import SearchStatus from './components/SearchStatus'
 import api from './api'
 
 function App() {
-	const [users, setUsers] = useState(api.users.fetchAll())
-	const handleDelete = (userId) => {
-		setUsers(users.filter((user) => user._id !== userId))
-	}
-	const handleToggleBookMark = (id) => {
-		setUsers(
-			users.map((user) => {
-				if (user._id === id) {
-					return { ...user, bookmark: !user.bookmark }
-				}
-				return user
-			})
-		)
-		console.log(id)
-	}
-	return (
-		<div>
-			<SearchStatus length={users.length} />
-			<Users
-				users={users}
-				onDelete={handleDelete}
-				onToggleBookMark={handleToggleBookMark}
-			/>
-		</div>
-	)
+    const [users, setUsers] = useState(api.users.fetchAll())
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId))
+    }
+    const handleToggleBookMark = (id) => {
+        setUsers(
+            users.map((user) => {
+                if (user._id === id) {
+                    return { ...user, bookmark: !user.bookmark }
+                }
+                return user
+            })
+        )
+        console.log(id)
+    }
+    return (
+        <div>
+            <SearchStatus length={users.length} />
+            <Users
+                users={users}
+                onDelete={handleDelete}
+                onToggleBookMark={handleToggleBookMark}
+            />
+        </div>
+    )
 }
 
 export default App
